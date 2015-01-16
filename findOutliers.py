1fb3b1f7123dd6aed0aede38ae395180923f1710
andyschultz <andy.schultz1@gmail.com>
2015-01-16 12:57:40 -0500

    Remove Large Files

--- findOutliers.py
+++ findOutliers.py
@@ -4,7 +4,7 @@ import pandas as pd,sys,os
 
 
 file = sys.argv[1] #Change this to take a system argument
-threshold = 10
+threshold = 50
 
 
 #Automatically removes lines starting with '#' except for the first line
@@ -14,9 +14,11 @@ with open(file,"w") as output:
     with open("temp.txt") as f:
         for line in f:
             if i == 0:
-                output.write(line)
+                output.write(line.replace('# Time\tElapsed Time','Timestamp\tTimeElapsed'))
             elif line.startswith('#'):
-                print("Removed line "+str(i))
+                print("Removed header line "+str(i))
+            elif line.endswith('0.0000\n'):
+                print("Removed bad reading line "+str(i))
             else:
                 output.write(line)
             i += 1
@@ -28,7 +30,20 @@ os.remove("temp.txt")
 
 def remove_outliers(file):
     df = pd.read_csv(file,sep='\t')
-    df.iloc[:,2:df.shape[1]]=df.iloc[:,2:df.shape[1]].where(df.iloc[:,2:df.shape[1]] >threshold) # df.shape[1] is the number of columns
+    df.iloc[:,2:df.shape[1]]=df.iloc[:,2:df.shape[1]].where(df.iloc[:,2:df.shape[1]] >threshold)
     df.to_csv(file,index=False,sep="\t",na_rep="NaN")
     
 remove_outliers(file)
+
+os.rename(file,"temp.txt")
+with open(file,"w") as output:
+    i = 0
+    with open("temp.txt") as f:
+        for line in f:
+            if i == 0:
+                output.write(line.replace('Timestamp\tTimeElapsed','Date\tTime\tElapsed'))
+            else:
+                output.write(line)
+            i += 1
+os.remove("temp.txt")
+    
